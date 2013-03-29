677db08dd995d5f4ccef7f8c8673e7ecf71d767b
Mark Plomer <mail@mark-plomer.de>
2013-03-29 01:03:27 +0100

    Changed scanner format to binary

--- midi-editor/ScannerRenderer.php
+++ midi-editor/ScannerRenderer.php
@@ -1,29 +1,18 @@
 <?php
 
 class ScannerRenderer extends NoteRenderer {
-    public $microsecondsOverhead = 76;
-
     public function render($midiFile) {
         $scannerData = '';
 
         $notes = $this->renderNotes($midiFile);
 
-        $currentPosition = 0;
-        $direction = 1;
-
         foreach ($notes as $note) {
-            $pause = $note->pause;
-            $steps = round($note->duration * $note->frequency / 1000000);
-            $delay = max(round(1000000 / $note->frequency) - $this->microsecondsOverhead, 0);
+            $packet = '';
+            $packet .= pack('L', $note->pause);
+            $packet .= pack('L', $note->frequency);
+            $packet .= pack('L', $note->duration);
 
-            $scannerData .= '{' . $pause .', ' . $steps . ', ' . $delay .', ' . ($direction == 1 ? '1' : '0') .'},' . PHP_EOL;
-
-            $currentPosition += $steps * $direction;
-            if ($currentPosition >= 2000) {
-                $direction = -1;
-            } elseif ($currentPosition <= 0) {
-                $direction = 1;
-            }
+            $scannerData .= $packet;
         }
 
         return $scannerData;
